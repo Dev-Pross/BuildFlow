391e077b7cd77526efccbc6d1ab6c07732f0204c
Vamsi_0 <iamvamsi0@gmail.com>
2025-11-13 11:34:18 +0530

    Setting UP Done

--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
+
+    
     <NextThemesProvider
       attribute="class"
       defaultTheme="system"
