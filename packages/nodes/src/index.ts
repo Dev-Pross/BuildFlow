0de31c581a13506e209a325863381ac5941937dd
Vamsi_0 <iamvamsi0@gmail.com>
2025-11-27 13:19:04 +0530

    Fixed

--- packages/nodes/src/index.ts
+++ packages/nodes/src/index.ts
@@ -1,4 +1,8 @@
 // Export everything from one place
-export * from './registry/node-registry';
-export * from './google-sheets/google-sheets.node';
-export * from './common/google-oauth-service';
+// export * from './registry/node-registry';
+// export * from './google-sheets/google-sheets.node';
+// export * from './common/google-oauth-service';
+
+
+console.log("Hello World From node / index.hs")
+
--- packages/nodes/src/registry/node-registry.ts
+++ packages/nodes/src/registry/node-registry.ts
@@ -40,4 +40,4 @@ class NodeRegistry{
     }
 }
 
-export { NodeRegistry }
+export default NodeRegistry;
